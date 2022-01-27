29ae3bf4c2969dae79d044edcfd1b772f24304cf
Gleb Pilipenka <g.pilipenko@itransition.com>
2022-01-27 11:21:59 +0300

    start app, add .env, fix .gitignore

--- server/index.js
+++ server/index.js
@@ -1,1 +1,17 @@
-// Server here...
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const PORT = process.env.APP_PORT || 5000;
+const app = express();
+
+const startApp = async () => {
+  try {
+    app.listen(PORT, () => console.log(`Server was started on PORT = ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startApp();
