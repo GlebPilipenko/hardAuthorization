9bce01949f9a70ec12ca3fbf5c61f99e876e7e5a
Gleb Pilipenka <g.pilipenko@itransition.com>
2022-01-27 10:26:05 +0300

    add eslint, prettier config, setting absolute paths

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,3 @@
-import React from 'react';
+import { FC } from 'react';
 
-function App() {
-  return (
-    <h1>Title</h1>
-  );
-}
-
-export default App;
+export const App: FC = () => <h1>MERN</h1>;
